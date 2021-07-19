b7c975fa09a56f431ce8b2b6ee79f1099427ef06
dudenayak <deveshjain2k1@gmail.com>
2021-07-20 00:31:57 +0530

    InfoSection Created

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+  paragraphTwo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
